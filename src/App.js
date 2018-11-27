0173221244aa20fddf756ad58dae88161b9422e0
kubzer0 <jakub.gryka@gmail.com>
2018-11-27 14:03:02 +0100

    added listItems and delete icon, it deletes record from database and loades it gain to browser

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
+import { List, ListItem } from 'material-ui/List'
+import Checkbox from 'material-ui/Checkbox'
+import DeleteIcon from 'material-ui/svg-icons/action/delete'
 
 const API_URL = 'https://poniedzialek-5c108.firebaseio.com'
 
-const style = {
-  margin: 12,
-}
 
 class App extends React.Component {
 
@@ -15,25 +15,25 @@ class App extends React.Component {
     taskName: ""
   }
 
-addTask=()=>{
-  if (this.state.taskName !== '') {
-    let tasks = this.state.tasks
-    const newTask = ({ taskName: this.state.taskName, completed: false })
-    fetch(`${API_URL}/tasks.json`, {
-      method: 'POST',
-      body: JSON.stringify(newTask)
-    })
-      .then(response => response.json())
-      .then((data) => {
-        newTask.id = data.name
-        tasks.push(newTask)
-        this.setState({ tasks: tasks, taskName: "" })
+  addTask = () => {
+    if (this.state.taskName !== '') {
+      let tasks = this.state.tasks
+      const newTask = ({ taskName: this.state.taskName, completed: false })
+      fetch(`${API_URL}/tasks.json`, {
+        method: 'POST',
+        body: JSON.stringify(newTask)
       })
+        .then(response => response.json())
+        .then((data) => {
+          newTask.id = data.name
+          tasks.push(newTask)
+          this.setState({ tasks: tasks, taskName: "" })
+        })
+    }
   }
-}
 
   handleClick = (event) => {
-  this.addTask()
+    this.addTask()
   }
 
   handleChange = (event) => {
@@ -41,15 +41,21 @@ addTask=()=>{
   }
 
   handleKeyDown = (event) => {
-    if (event.keyCode === 13)
-    {this.addTask()}
+    if (event.keyCode === 13) { this.addTask() }
   }
 
-  componentWillMount() {
+  handleDelete =(id) =>{
+    fetch(`${API_URL}/tasks/${id}.json`, {
+      method: 'DELETE'
+    })
+    .then(()=> this.loadData())
+  }
+
+  loadData= ()=>{
     fetch(`${API_URL}/tasks.json`)
       .then(response => response.json())
       .then(data => {
-        if (!data){
+        if (!data) {
           return
         }
         const array = Object.entries(data)
@@ -59,6 +65,10 @@ addTask=()=>{
         })
         this.setState({ tasks: taskList })
       })
+  }
+
+  componentWillMount() {
+    this.loadData()
   }
 
   render() {
@@ -74,19 +84,19 @@ addTask=()=>{
         <RaisedButton
           label="+"
           primary={true}
-          style={style}
           onClick={this.handleClick}
         />
-        <div>
+        <List>
           {this.state.tasks.map((task, index) =>
             (
-              <div
+              <ListItem
                 key={task.id}
-              >
-                {task.taskName}
-              </div>
+                primaryText={task.taskName}
+                leftCheckbox={<Checkbox />}
+                rightIcon={<DeleteIcon onClick={()=>this.handleDelete(task.id)}/>}
+              />
             ))}
-        </div>
+        </List>
       </div>
     )
   }
