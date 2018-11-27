08e29b0ef804b2bbbe01f6f94abbe7cdec5aa6f4
kubzer0 <jakub.gryka@gmail.com>
2018-11-27 14:54:20 +0100

    Updates

---
+++ precache-manifest.96f28392cbffd3628e846c00103b2e35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "5adcb99843830393e4cc",
+    "url": "/todo-app/static/js/main.5adcb998.chunk.js"
+  },
+  {
+    "revision": "fb4a947b1333c8556745",
+    "url": "/todo-app/static/js/1.fb4a947b.chunk.js"
+  },
+  {
+    "revision": "5adcb99843830393e4cc",
+    "url": "/todo-app/static/css/main.4b51b0f0.chunk.css"
+  },
+  {
+    "revision": "0fa9d0fc192f6d17bf3686565fda3bfd",
+    "url": "/todo-app/index.html"
+  }
+];
