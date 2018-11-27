54f2f3a9c5b7c7f95f1ff2fe00dfde89f6e40894
kubzer0 <jakub.gryka@gmail.com>
2018-11-27 11:01:49 +0100

    textfield and button with logic added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton'
 
-class App extends Component {
+
+
+const style = {
+  margin: 12,
+}
+
+class App extends React.Component {
+
+  state = {
+    tasks: [
+      {
+        taskName: 'odkurzanie',
+        completed: false
+      },
+      {
+        taskName: 'zmywanie',
+        completed: false
+      }
+    ],
+    taskName: ""
+  }
+
+  handleClick = (event) => {
+    if (this.state.taskName !== '') {
+      let tasks = this.state.tasks
+      tasks.push({ taskName: this.state.taskName, completed: false })
+      this.setState({ tasks: tasks, taskName: "" })
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({ taskName: event.target.value })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <TextField
+          hintText="Add Task"
+          value={this.state.taskName}
+          onChange={this.handleChange}
+        >
+        </TextField>
+        <RaisedButton
+          label="+"
+          primary={true}
+          style={style}
+          onClick={this.handleClick}
+        />
+        <div>
+          {this.state.tasks.map((task, index) =>
+            (
+              <div>
+                {task.taskName}
+              </div>
+            ))}
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
